121771981f8dd4307c75fdca8cb42935afbe2a15
rishi-masurkar01 <156550439+rishi-masurkar01@users.noreply.github.com>
2026-04-12 10:09:59 +0530

    Bulk sync: 2 submissions

---
+++ Data Structures & Algorithms/duplicate-integer/submission-1.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean hasDuplicate(int[] nums) {
+      HashSet<Integer> hash = new HashSet<>();
+      for(int num : nums)
+      {
+        if(hash.contains(num))
+        {
+            return true;
+        }
+        hash.add(num);
+      }
+      return false;
+    }
+}
